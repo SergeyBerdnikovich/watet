1cfabf4bb0225b0f4c65bd9b24f14e1623b90b1e
SergeyBerdnikovich <atom@tut.by>
2013-05-24 20:39:27 +0300

    fixed list items link in meiler

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -2,7 +2,12 @@ class Mailer < ActionMailer::Base
   default :from => "dev.dvporg@gmail.com"
 
   def send_list_items_link(user, email)
-    @link = "http://watet.dvporg.com/list_items?#{user.id}}"
+    if user.slug.blank?
+      @link = "http://watet.dvporg.com/list_items?#{user.id}"
+    else
+      @link = "http://watet.dvporg.com/list_items?#{user.slug}"
+    end
+
     @user = user
     @to_email = email
     mail(:to => @to_email, :subject => "list items from #{@user.email}")
