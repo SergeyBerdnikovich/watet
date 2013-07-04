1ac4f7175f20fe2b0a13f2915e77b4cedc995d5d
SergeyBerdnikovich <atom@tut.by>
2013-07-04 14:47:42 +0300

    add loading.gif for add images, upated css for images

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '307946899338802', 'fcc14b76bdf41b84a808965221f86d75'
-  #provider :facebook, '394395114009962', 'eb24e88ffb2fc60952d293dd30205982', #localhost:3000
+  #provider :facebook, '307946899338802', 'fcc14b76bdf41b84a808965221f86d75'
+  provider :facebook, '394395114009962', 'eb24e88ffb2fc60952d293dd30205982', #localhost:3000
   {
    :scope => "email",
    :secure_image_url => true,
@@ -8,8 +8,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
    :display => 'popup',
    :provider_ignores_state => true
   }
-  #provider :google_oauth2, '127127904403-qi1ted9kql4vtj04u9149lki8t841up4.apps.googleusercontent.com', 'vEOmAOmfURqzpBsKd4aKEE1A', #localhost:3000
-  provider :google_oauth2, '437662557322-itghbd3ciicb4tal8c9djtu9aqf0lnpt.apps.googleusercontent.com', 'nh2oTryc3-oMNtbH85cReNkq', #https://watet.dvporg.com
+  provider :google_oauth2, '127127904403-qi1ted9kql4vtj04u9149lki8t841up4.apps.googleusercontent.com', 'vEOmAOmfURqzpBsKd4aKEE1A', #localhost:3000
+  #provider :google_oauth2, '437662557322-itghbd3ciicb4tal8c9djtu9aqf0lnpt.apps.googleusercontent.com', 'nh2oTryc3-oMNtbH85cReNkq', #https://watet.dvporg.com
   {
    :scope => "userinfo.email,userinfo.profile,plus.login",
    :approval_prompt => "force",
