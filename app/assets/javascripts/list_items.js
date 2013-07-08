c8e25071c92249e8716b115c3e7536a415a95d99
Dvporg <dvporg@gmail.com>
2013-07-08 15:52:58 +0000

    dvp updated sortable

--- app/assets/javascripts/list_items.js
+++ app/assets/javascripts/list_items.js
@@ -1,28 +1,30 @@
+var drag_started = false;
+
 $(document).ready(function() {
 
-$('.deleter').bind('ajax:success', function() {
+	$('.deleter').bind('ajax:success', function() {
 
-list_item_id = $(this).attr('list_item_id')
-$('#'+list_item_id).remove()
-$(window).resize()
-});
+		list_item_id = $(this).attr('list_item_id')
+		$('#'+list_item_id).remove()
+		$(window).resize()
+	});
 
 
 	$('.banner').mouseenter(function(){
-$(this).attr('class', 'banner_active')
+		$(this).attr('class', 'banner_active')
 	})
-		$('.banner').mouseleave(function(){
+	$('.banner').mouseleave(function(){
 
-			$(this).attr('class', 'banner')
+		$(this).attr('class', 'banner')
 
-		})
-$(window).resize()
+	})
+	$(window).resize()
 
 	$('.li').mouseenter(function(){
 
 		current_class = $(this).attr('class')
 		may_change = true
-			if (current_class.indexOf("list_item_active") != -1){
+		if (current_class.indexOf("list_item_active") != -1){
 			may_change = false
 		}
 
@@ -59,17 +61,21 @@ $(window).resize()
 
 	$('.li').click(function(e) {
 		if(e.target.className.indexOf('toggler') != -1){
-			current_class = $(this).attr('class')
-		if (current_class != "list_item_active"){
-			$(this).attr('class', 'list_item_active')
-		} else{
-			$(this).attr('class', 'list_item_hover')
-		}
+			if (drag_started == false){
+				current_class = $(this).attr('class')
+				if (current_class != "list_item_active"){
+					$(this).attr('class', 'list_item_active')
+				} else{
+					$(this).attr('class', 'list_item_hover')
+				}
+			} else {
+				drag_started = false
+			}
 		}
 	});
 
 
-setTimeout('$(window).resize()',500); //small fix 
+	setTimeout('$(window).resize()',500); //small fix
 
 
 });
