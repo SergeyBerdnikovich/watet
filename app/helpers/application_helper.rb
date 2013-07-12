46cd1b1f9c4981d9808372acebd3d9a1a09bfae0
SergeyBerdnikovich <atom@tut.by>
2013-07-12 12:38:10 +0300

    updated helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,10 @@
 module ApplicationHelper
   def get_avatar
-    if current_user
-      str = ""
-      if image_tag(current_user.try(:profile).try(:avatar))
-        str = current_user.try(:profile).try(:avatar)
-        str = "" if str == nil
-      end
-    else
-      str = '/assets/maket/no_avatar.png'
-    end
-    str = '/assets/maket/no_avatar.png' if str == ""
-    image_tag(str)
-
+    ava = current_user.try(:profile).try(:avatar)
+    ava = '/assets/maket/no_avatar.png' if ava.blank?
+    image_tag ava
   end
+
   def show_page(name, with_title = false, with_latest_update = false)
     page = StaticPage.find_by_name(name)
     page ? text = page.content : page = StaticPage.create!(:name => name)
@@ -21,8 +13,8 @@ module ApplicationHelper
     text = "<center><h2>#{page[:title]}</h2></center> <p> #{text}" if with_latest_update
 
     text
-
   end
+
   def link_to_page(name)
     page = StaticPage.find_by_name(name)
     page = StaticPage.create!(:name => name) unless page
@@ -53,15 +45,15 @@ module ApplicationHelper
 
   def list_item_desc(list_item)
     if list_item.description
-    regexp = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
-    list_item.description.gsub(regexp).each do |result|
-      if result =~ /^http:\/\/[a-z1-9.\/@A-Z?=]+/
-        "<a href=#{result}>#{result}</a>"
-      else
-        "<a href=#{'http://' + result}>#{result}</a>"
+      regexp = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
+      list_item.description.gsub(regexp).each do |result|
+        if result =~ /^http:\/\/[a-z1-9.\/@A-Z?=]+/
+          "<a href=#{result}>#{result}</a>"
+        else
+          "<a href=#{'http://' + result}>#{result}</a>"
+        end
       end
     end
-  end
   end
 
   def banner_image_url(banner)
